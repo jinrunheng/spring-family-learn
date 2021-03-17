1d34018e2724ea42582b2da333ae9fffdeb875be
jinrunheng <1175088275@qq.com>
2021-03-18 01:48:57 +0800

    finished Chapter3 mybatis-demo

--- Chapter3/mybatis-demo/src/main/java/com/geektime/mybatisdemo/mapper/CoffeeMapper.java
+++ Chapter3/mybatis-demo/src/main/java/com/geektime/mybatisdemo/mapper/CoffeeMapper.java
@@ -1,9 +1,7 @@
 package com.geektime.mybatisdemo.mapper;
 
 import com.geektime.mybatisdemo.model.Coffee;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface CoffeeMapper {
@@ -11,5 +9,8 @@ public interface CoffeeMapper {
     int save(Coffee coffee);
 
     @Select("select * from t_coffee where id = #{id}")
+    @Results({
+            @Result(id=true,column = "id",property = "id")
+    })
     Coffee findById(@Param("id") Long id);
 }
