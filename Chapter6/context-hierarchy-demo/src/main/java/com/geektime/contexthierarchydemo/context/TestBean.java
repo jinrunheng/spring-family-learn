2ab3a1fa3212c6b583ce151b1469cc0225e3551b
jinrunheng <1175088275@qq.com>
2021-04-03 20:58:57 +0800

    finished context-hierarchy-demo

---
+++ Chapter6/context-hierarchy-demo/src/main/java/com/geektime/contexthierarchydemo/context/TestBean.java
@@ -0,0 +1,15 @@
+package com.geektime.contexthierarchydemo.context;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@AllArgsConstructor
+@Slf4j
+public class TestBean {
+
+    private String context;
+
+    public void hello() {
+        log.info("hello " + context);
+    }
+}
---
+++ Chapter6/context-hierarchy-demo/src/test/java/com/geektime/contexthierarchydemo/ContextHierarchyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geektime.contexthierarchydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextHierarchyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
