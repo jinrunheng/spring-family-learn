41d2fa8c67ac38add0bd6fc3e171066354cd843c
jinrunheng <1175088275@qq.com>
2021-04-14 10:22:42 +0800

    finished hateoas-customer-service

--- Chapter8/hateoas-customer-service/src/main/java/com/geektime/hateoascustomerservice/CustomerRunner.java
+++ Chapter8/hateoas-customer-service/src/main/java/com/geektime/hateoascustomerservice/CustomerRunner.java
@@ -1,22 +1,27 @@
 package com.geektime.hateoascustomerservice;
 
 import com.geektime.hateoascustomerservice.model.Coffee;
+import com.geektime.hateoascustomerservice.model.CoffeeOrder;
+import com.geektime.hateoascustomerservice.model.OrderState;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.Collections;
 
 @Component
 @Slf4j
@@ -31,6 +36,11 @@ public class CustomerRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         Link coffeeLink = getLink(ROOT_URI, "coffees");
         readCoffeeMenu(coffeeLink);
+        Resource<Coffee> americano = addCoffee(coffeeLink);
+
+        Link orderLink = getLink(ROOT_URI, "coffeeOrders");
+        addOrder(orderLink, americano);
+        queryOrders(orderLink);
     }
 
     private void readCoffeeMenu(Link coffeeLink) {
@@ -52,5 +62,41 @@ public class CustomerRunner implements ApplicationRunner {
         Link link = rootResp.getBody().getLink(rel);
         log.info("Link : {}", link);
         return link;
+    }
+
+    private Resource<Coffee> addCoffee(Link link) {
+        Coffee americano = Coffee.builder()
+                .name("americano")
+                .price(Money.of(CurrencyUnit.of("CNY"), 25.00))
+                .build();
+
+        RequestEntity<Coffee> req = RequestEntity.post(link.getTemplate().expand()).body(americano);
+        ResponseEntity<Resource<Coffee>> resp = restTemplate.exchange(req, new ParameterizedTypeReference<Resource<Coffee>>() {
+        });
+        log.info("add Coffee Response : {}", resp);
+        return resp.getBody();
+    }
+
+    private void addOrder(Link link, Resource<Coffee> coffee) {
+        CoffeeOrder newOrder = CoffeeOrder.builder()
+                .customer("Li Lei")
+                .state(OrderState.INIT)
+                .build();
+
+        RequestEntity<?> req = RequestEntity.post(link.getTemplate().expand()).body(newOrder);
+        ResponseEntity<Resource<CoffeeOrder>> resp = restTemplate.exchange(req, new ParameterizedTypeReference<Resource<CoffeeOrder>>() {
+        });
+        log.info("add Order Response : {}", resp);
+
+        Resource<CoffeeOrder> order = resp.getBody();
+        Link items = order.getLink("items");
+        req = RequestEntity.post(items.getTemplate().expand()).body(Collections.singletonMap("_links", coffee.getLink("self")));
+        ResponseEntity<String> itemResp = restTemplate.exchange(req, String.class);
+        log.info("add Order Items Response : {}", itemResp);
+    }
+
+    private void queryOrders(Link link) {
+        ResponseEntity<String> resp = restTemplate.getForEntity(link.getTemplate().expand(), String.class);
+        log.info("query Order Response : {}", resp);
     }
 }
