81409bafb4ddccd2c7bc7dd676e79bf8a10a52cd
jinrunheng <1175088275@qq.com>
2021-04-18 21:18:10 +0800

    finished geektime.autoconfigure-backport

--- Chapter9/autoconfigure-demo/src/main/java/geektime/spring/hello/AutoconfigureDemoApplication.java
+++ Chapter9/autoconfigure-demo/src/main/java/geektime/spring/hello/AutoconfigureDemoApplication.java
@@ -1,4 +1,5 @@
-package geektime.spring.hello.autoconfiguredemo;
+package geektime.spring.hello;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
